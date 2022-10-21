0e88fbb4670d501af780d930ef72eaf9d1712911
Elena Gracheva <elena.gracheva.639@gmail.com>
2022-10-21 20:57:37 +0500

    added loader component

---
+++ project/src/components/spinner/spinner.tsx
@@ -0,0 +1,12 @@
+import './spinner.css';
+
+function Spinner(): JSX.Element {
+  return (
+    <div className="spinner">
+      <div className="spinner__circle"></div>
+    </div>
+
+  );
+}
+
+export default Spinner;
