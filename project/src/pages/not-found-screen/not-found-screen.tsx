7ee28a77955916deacc8c07b5a1007c229c017ce
Elena Gracheva <elena.gracheva.639@gmail.com>
2022-10-28 16:50:57 +0500

    404 page styles

--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,19 +1,17 @@
 import { Link } from 'react-router-dom';
 import './not-found-page.css';
+import Header from '../../components/header/header';
 
 function NotFoundScreen(): JSX.Element {
   return (
     <div className="not-found-page">
-      <div className="page-content">
+      <Header />
 
-        <div className="not-found-page__content">
-          <p className="not-found-page__text">404 - page not found</p>
-          <Link className="not-found-page__link" to='/'>
-            Return to main page
-          </Link>
-        </div>
-
-
+      <div className="not-found-page__content">
+        <p className="not-found-page__text">404 - page not found</p>
+        <Link className="not-found-page__link" to='/'>
+          Return to main page
+        </Link>
       </div>
     </div>
   );
